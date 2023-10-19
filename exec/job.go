52c8ef9589332a73af46b44ba4e2b06f48c5f37e
Soderasen AU <soderasen.au@outlook.com>
2023-10-20 09:20:59 +1100

    feat: support NextRun info in task

--- exec/job.go
+++ exec/job.go
@@ -8,18 +8,19 @@ import (
 type (
 	// App can do a Task for many times, each time of the task-execution is a Job
 	Task struct {
-		Name        string `json:"name,omitempty" yaml:"name,omitempty" bson:"name,omitempty"`
-		LastJob     *Job   `json:"last_job,omitempty" yaml:"last_job,omitempty" bson:"last_job,omitempty" gorm:"serializer:json"`
-		LastSucc    *Job   `json:"last_succ,omitempty" yaml:"last_succ,omitempty" bson:"last_succ,omitempty" gorm:"serializer:json"`
-		LastFail    *Job   `json:"last_fail,omitempty" yaml:"last_fail,omitempty" bson:"last_fail,omitempty" gorm:"serializer:json"`
-		History     []*Job `json:"history,omitempty" yaml:"history,omitempty" bson:"history,omitempty" gorm:"serializer:json"`
-		MaxJobCount int    `json:"max_job_count,omitempty" yaml:"max_job_count,omitempty" bson:"max_job_count,omitempty"`
+		Name        string     `json:"name,omitempty" yaml:"name,omitempty" bson:"name,omitempty"`
+		LastJob     *Job       `json:"last_job,omitempty" yaml:"last_job,omitempty" bson:"last_job,omitempty" gorm:"serializer:json"`
+		LastSucc    *Job       `json:"last_succ,omitempty" yaml:"last_succ,omitempty" bson:"last_succ,omitempty" gorm:"serializer:json"`
+		LastFail    *Job       `json:"last_fail,omitempty" yaml:"last_fail,omitempty" bson:"last_fail,omitempty" gorm:"serializer:json"`
+		History     []*Job     `json:"history,omitempty" yaml:"history,omitempty" bson:"history,omitempty" gorm:"serializer:json"`
+		NextRun     *time.Time `json:"next_run,omitempty" yaml:"next_run,omitempty" bson:"next_run,omitempty"`
+		MaxJobCount int        `json:"max_job_count,omitempty" yaml:"max_job_count,omitempty" bson:"max_job_count,omitempty"`
 	}
 
 	// A Job finishes a Task using a Request
 	Job struct {
 		TaskName   string         `json:"task_name,omitempty" yaml:"task_name,omitempty" bson:"task_name,omitempty" gorm:"index"`
-		ReqID      string         `json:"req_id,omitempty" yaml:"req_id,omitempty" bson:"req_id,omitempty"`
+		ReqID      string         `json:"req_id,omitempty" yaml:"req_id,omitempty" bson:"req_id,omitempty" gorm:"index"`
 		Status     Status         `json:"status,omitempty" yaml:"status,omitempty" bson:"status,omitempty"`
 		StartedBy  string         `json:"started_by,omitempty" yaml:"started_by,omitempty" bson:"started_by,omitempty"`
 		StartedAt  time.Time      `json:"started_at,omitempty" yaml:"started_at,omitempty" bson:"started_at,omitempty"`
