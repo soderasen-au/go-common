fd7bfa5c87354c556629bbd00b30585d7ab8b601
Soderasen AU <soderasen.au@outlook.com>
2025-03-26 11:19:47 +1100

    chore: Add GetLoggerWithCloser

--- loggers/global.go
+++ loggers/global.go
@@ -70,6 +70,38 @@ func GetLogger(fn string) (*zerolog.Logger, error) {
 	return &logger, nil
 }
 
+type LogFileCloser func() error
+
+func GetLoggerWithCloser(fn string) (*zerolog.Logger, LogFileCloser, error) {
+	writers := []io.Writer{&NullWriter{}}
+	if !EnableConsolColor {
+		DefaultConsoleWriter.NoColor = true
+	}
+	if EnableConsolWriter {
+		writers = append(writers, DefaultConsoleWriter)
+	}
+	var fnWriteCloser io.WriteCloser
+	if EnableFileWriter && len(fn) > 0 {
+		fnWriteCloser = &lumberjack.Logger{
+			Filename:   fn,
+			MaxSize:    globalConfig.MaxSizeMB,
+			MaxAge:     globalConfig.MaxAgeDays,
+			MaxBackups: globalConfig.MaxBackups,
+			LocalTime:  globalConfig.UseLocalTime,
+			Compress:   globalConfig.Compress,
+		}
+		writers = append(writers, fnWriteCloser)
+	}
+	multi := zerolog.MultiLevelWriter(writers...)
+	logger := zerolog.New(multi).With().Timestamp().Logger()
+	return &logger, func() error {
+		if fnWriteCloser != nil {
+			return fnWriteCloser.Close()
+		}
+		return nil
+	}, nil
+}
+
 func NewLogger(c Config) *zerolog.Logger {
 	writers := []io.Writer{&NullWriter{}}
 	if !c.EnableConsolColor {
